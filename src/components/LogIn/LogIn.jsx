157ea2084b1c5d9cc03d347c306c23a79e15efc9
Alina Noor <AN10_23@hotmail.com>
2024-08-16 16:20:18 -0400

    add initial structures and styling

---
+++ src/components/LogIn/LogIn.jsx
@@ -0,0 +1,14 @@
+import "./LogIn.scss";
+
+
+const LogIn = () => {
+
+    return(
+        <>
+        <div className="LogIn">
+
+        </div>
+        </>
+    )
+};
+export default LogIn;
---
+++ src/components/SignUp/SignUp.jsx
@@ -0,0 +1,16 @@
+import "./SignUp.scss";
+
+
+const SignUp = () => {
+
+    return(
+        <>
+        <div className="SignUp">
+            <div className="SignUp__container">
+
+            </div>
+        </div>
+        </>
+    )
+};
+export default SignUp;
