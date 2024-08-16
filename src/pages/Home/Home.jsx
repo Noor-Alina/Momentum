04aaeaa8d28928ee9c87fef84795967b13c3284b
Alina Noor <AN10_23@hotmail.com>
2024-08-16 11:58:16 -0400

    start home page, add the header component and style background

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,16 @@
+import "./Home.scss";
+import Header from "../../components/Header/Header";
+
+
+const Home = () => {
+
+    return(
+        <>
+        <Header/>
+        <div className="Home">
+
+        </div>
+        </>
+    )
+};
+export default Home;
