c4838647b7a57a0507dbb56a5f265896d9925367
Alina Noor <AN10_23@hotmail.com>
2024-08-16 11:59:57 -0400

    create routing to base pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
-import Header from "./components/Header/Header";
+import SignUp from "./pages/SignUp/SignUp";
+import LogIn from "./pages/LogIn/LogIn";
 
 function App() {
   return (
     <BrowserRouter>
-      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/SignUp" element={<SignUp />} />
+        <Route path="/LogIn" element={<LogIn />} />
       </Routes>
     </BrowserRouter>
   );
