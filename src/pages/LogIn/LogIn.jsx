6ce2362442f4cc6f85631c867bd54f958bb93477
Alina Noor <AN10_23@hotmail.com>
2024-08-16 11:59:11 -0400

    create initial logIn and SignUp structures and add Header

---
+++ src/pages/LogIn/LogIn.jsx
@@ -0,0 +1,15 @@
+import Header from "../../components/Header/Header";
+
+
+const LogIn = () => {
+
+    return(
+        <>
+        <Header/>
+        <div className="LogIn">
+
+        </div>
+        </>
+    )
+};
+export default LogIn;
---
+++ src/pages/SignUp/SignUp.jsx
@@ -0,0 +1,15 @@
+import Header from "../../components/Header/Header";
+
+
+const SignUp = () => {
+
+    return(
+        <>
+        <Header/>
+        <div className="SignUp">
+
+        </div>
+        </>
+    )
+};
+export default SignUp;
