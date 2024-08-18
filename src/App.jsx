ae803dfc60b504e961880253541a6b6fb29839d8
Alina Noor <AN10_23@hotmail.com>
2024-08-17 21:56:01 -0400

    update app so Auth provider and protected routes are applied

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,56 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+import ProtectedRoute from "./components/ProtectedRoute/ProtectedRoute";
 import HomePage from "./pages/HomePage/HomePage";
 import SignUpPage from "./pages/SignUpPage/SignUpPage";
 import LogInPage from "./pages/LogInPage/LogInPage";
-
+import WorkoutsPage from "./pages/WorkoutsPage/WorkoutsPage";
+import TrackingPage from "./pages/TrackingPage/TrackingPage";
+import MealPlanningPage from "./pages/MealPlanningPage/MealPlanningPage";
+import ProfilePage from "./pages/ProfilePage/ProfilePage";
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/SignUp" element={<SignUpPage />} />
-        <Route path="/LogIn" element={<LogInPage />} />
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/SignUp" element={<SignUpPage />} />
+          <Route path="/LogIn" element={<LogInPage />} />
+          <Route 
+            path="/Workouts" 
+            element={
+              <ProtectedRoute>
+                <WorkoutsPage />
+              </ProtectedRoute>
+              } 
+          />
+          <Route 
+            path="/Tracking" 
+            element={
+              <ProtectedRoute>
+                <TrackingPage />
+              </ProtectedRoute>
+              } 
+          />
+          <Route 
+            path="/MealPlanning" 
+            element={
+              <ProtectedRoute>
+                <MealPlanningPage />
+              </ProtectedRoute>
+              } 
+          />
+          <Route 
+            path="/Profile" 
+            element={
+              <ProtectedRoute>
+                <ProfilePage />
+              </ProtectedRoute>
+              } 
+          />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
