87cb26b256ec86a60fd4647740cad5afd6232345
Alina Noor <AN10_23@hotmail.com>
2024-08-18 00:57:35 -0400

    chnage header text and add conditional rendering to the hero button

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,8 +2,10 @@ import "./Home.scss";
 import { Link } from "react-router-dom";
 import HeroGraphic from "../../assets/images/Momentum_hero.svg";
 import Animate from "../Animate/Animate";
+import { useAuth } from "../../context/AuthContext";
 
 const Home = () => {
+    const {isLoggedIn} = useAuth();
 
     return(
         <>
@@ -11,13 +13,19 @@ const Home = () => {
             <div className="Home__hero">
                 <div className="Home__hero-heading">
                     <h1 className="Home__hero-heading-text">
-                        Momentum Fitness Hub
+                        Your journey to a healthier lifestyle starts here
                     </h1>
-                    <Link to="/LogIn"
+                    {!isLoggedIn() ? (<Link to="/LogIn"
                         className="Home__hero-heading-button"
                         >
                         JOIN NOW
+                    </Link>) : (
+                        <Link to="/Profile"
+                        className="Home__hero-heading-button"
+                        >
+                        GO TO PROFILE
                     </Link>
+                    )}
                 </div>
                 <div className="Home__hero-graphic">
                     <img className="Home__hero-graphic-image" src={HeroGraphic} alt="Momentum_hero_image" />
