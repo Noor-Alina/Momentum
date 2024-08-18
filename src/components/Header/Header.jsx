acb49cbb69f5522552e9164a5429e33dbedf0804
Alina Noor <AN10_23@hotmail.com>
2024-08-18 01:00:28 -0400

    create and style header, add conditional rendering for logged in users and path check

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,20 @@
 import "./Header.scss";
 import MomentumLogo from "../../assets/logo/momentum_logo.svg";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
+import { useAuth } from "../../context/AuthContext"; // Import useAuth to access user state
 
 const Header = () => {
+    const location = useLocation();
+    const { isLoggedIn } = useAuth(); // Get user state from AuthContext
+
+    // Determine which page is currently active
+    const isHomePage = location.pathname === '/';
+    const isLoginPage = location.pathname === '/LogIn';
+    const isSignUpPage = location.pathname === '/SignUp';
+    const isProtectedPage = ['/Workouts', '/Tracking', '/MealPlanning', '/Profile'].includes(location.pathname);
+
     return (
-        <div className="Header">
+        <div className={`Header ${isLoginPage ? 'Header--login' : ''} ${isSignUpPage ? 'Header--signup' : ''} ${isProtectedPage ? 'Header--protected' : ''}`}>
             <div className="Header__logo">
                 <NavLink to="/">
                     <img 
@@ -16,30 +26,79 @@ const Header = () => {
             </div>
             <div className="Header__links">
                 <ul className="Header__links-list">
-                    <li>
-                        <NavLink 
-                            to="/LogIn" 
-                            className={({isActive}) =>
-                                isActive ? "Header__item Header__item--active" : "Header__item"
-                            }
-                        >
-                          LogIn
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink 
-                            to="/SignUp" 
-                            className={({isActive}) =>
-                                isActive ? "Header__item Header__item--active" : "Header__item"
-                            }
-                        >
-                          SignUp
-                        </NavLink>
-                    </li>
+                    {/* Show login and signup links only if the user is not logged in */}
+                    {!isLoggedIn() ? (
+                        <>
+                            <li>
+                                <NavLink 
+                                    to="/LogIn" 
+                                    className={({ isActive }) =>
+                                       `${isActive ? "Header__item-auth  Header__item-auth--active" : ""} ${isSignUpPage ? "Header__item-auth" : ''} ${isHomePage ? "Header__item Header__item--home" : ""}`
+                                    }
+                                >
+                                  LogIn
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink 
+                                    to="/SignUp" 
+                                    className={({ isActive }) =>
+                                        ` ${isActive ? "Header__item-auth Header__item-auth--active" : ""} ${isLoginPage ? "Header__item-auth" : ''} ${isHomePage ? "Header__item Header__item--home" : ""}`
+                                    }
+                                >
+                                  SignUp
+                                </NavLink>
+                            </li>
+                        </>
+                    ) : (
+                        // Show protected links only if the user is logged in
+                        <>
+                            <li>
+                                <NavLink 
+                                    to="/Workouts" 
+                                    className={({ isActive }) =>
+                                        isActive ? "Header__item Header__item--active" : "Header__item"
+                                    }
+                                >
+                                  Workouts
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink 
+                                    to="/Tracking" 
+                                    className={({ isActive }) =>
+                                        isActive ? "Header__item Header__item--active" : "Header__item"
+                                    }
+                                >
+                                  Tracking
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink 
+                                    to="/MealPlanning" 
+                                    className={({ isActive }) =>
+                                        isActive ? "Header__item Header__item--active" : "Header__item"
+                                    }
+                                >
+                                  Meal Plans
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink 
+                                    to="/Profile" 
+                                    className={({ isActive }) =>
+                                        isActive ? "Header__item Header__item--active" : "Header__item"
+                                    }
+                                >
+                                  User Profile
+                                </NavLink>
+                            </li>
+                        </>
+                    )}
                 </ul>
             </div>
-
         </div>
-    )
+    );
 };
+
 export default Header;
