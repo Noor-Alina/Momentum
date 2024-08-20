2890b91ec27f74a62fe22adf2b9c338d20aafe8e
Alina Noor <AN10_23@hotmail.com>
2024-08-19 22:03:00 -0400

    create the meal plan display component to display the generated meal plan. Used in the MealPlanning component

---
+++ src/components/MealPlanDisplay/MealPlanDisplay.jsx
@@ -0,0 +1,39 @@
+import './MealPlanDisplay.scss';
+import React from 'react';
+
+
+const MealPlanDisplay = ({ mealPlan }) => {
+    console.log("in display component");
+
+
+    if (!mealPlan || mealPlan.length === 0) {
+        console.log("No meal plan");
+        return <p>No meal plans available.</p>;
+    }
+
+    return (
+        <div className="Meal-plan-display">
+            {mealPlan.map((dayData, dayIndex) => (
+                <div key={dayIndex}  className="Meal-plan-display__day">
+                    <h2 className="Meal-plan-display__day-heading">Meal Plan for {dayData.day.charAt(0).toUpperCase() + dayData.day.slice(1)}</h2>
+                    <div className="Meal-plan-display__meals">
+                        {dayData.meals.map((meal, mealIndex) => (
+                            <div key={mealIndex} className="Meal-plan-display__meal">
+                                <h3 className="Meal-plan-display__meal-type">{meal.meal.charAt(0).toUpperCase() + meal.meal.slice(1)}</h3>
+                                <p className="Meal-plan-display__meal-content"><strong>Name:</strong> {meal.name}</p>
+                                <p className="Meal-plan-display__meal-content"><strong>Ingredients:</strong></p>
+                                <ul className="Meal-plan-display__meal-ingrediant-list">
+                                    {meal.ingredients.map((ingredient, itemIndex) => (
+                                        <li className="Meal-plan-display__meal-ingrediant-item" key={itemIndex}>{ingredient}</li> // List each ingredient
+                                    ))}
+                                </ul>  
+                            </div> 
+                        ))}
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
+
+};
+export default MealPlanDisplay;
